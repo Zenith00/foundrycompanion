200146ae7a8d38c9581858f1a17034c28549fb37
Zenith <z@zenith.dev>
2021-06-28 12:46:06 -0700

    initial commit

--- scripts/module.js
+++ scripts/module.js
@@ -5,3 +5,9 @@ Hooks.once('init', async function() {
 Hooks.once('ready', async function() {
 
 });
+
+Hooks.on('renderTidy5eSheet', async function(app, html, data){
+  console.log(app);
+  console.log(html);
+  console.log(data);
+});
